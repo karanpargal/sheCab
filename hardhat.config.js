a200bc6562af43b3339146c04e5d60fed93bdcf1
Karan Pargal <karanpargal007@gmail.com>
2023-01-06 23:28:49 +0530

    :sparkles: Deploy smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-};
+  networks: {
+    hardhat: {
+    },
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
